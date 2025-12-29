5898fb06302b180e993d05bb8aba09c8ae23b5db
IceFerryLing <523918392@qq.com>
2025-12-29 18:17:30 +0800

    更改

---
+++ Aync/Server_demo.cpp
@@ -0,0 +1,27 @@
+#include "Server_demo.h"
+#include <iostream>
+#include <boost/asio.hpp>
+using namespace std;
+
+Server::Server(boost::asio::io_context& ioc, short port):_ioc(ioc)
+    ,_acceptor(ioc, tcp::endpoint(tcp::v4(), port)){
+    cout << "Server started on port: " << port << endl;
+    start_accept();
+}
+
+void Server::start_accept(){
+    shared_ptr<Session> new_session = make_shared<Session>(_ioc, this);
+
+    Session* new_session = new Session(_ioc, this);
+    _acceptor.async_accept(new_session->Socket(), std::bind(&Server::handle_accept, this, new_session, std::placeholders::_1));
+}
+
+void Server::handle_accept(shared_ptr<Session> new_session, const boost::system::error_code& error){
+    if(!error){
+        new_session->Start();
+    }else{
+        //delete new_session;
+    }
+    start_accept();
+}   
+
--- Aync/Session_demo.cpp
+++ Aync/Session_demo.cpp
@@ -34,22 +34,4 @@ void Session::handle_write(const boost::system::error_code& error){
     }
 }
 
-Server::Server(boost::asio::io_context& ioc, short port):_ioc(ioc)
-    ,_acceptor(ioc, tcp::endpoint(tcp::v4(), port)){
-    start_accept();
-}
-
-void Server::start_accept(){
-    Session* new_session = new Session(_ioc);
-    _acceptor.async_accept(new_session->Socket(), std::bind(&Server::handle_accept, this, new_session, std::placeholders::_1));
-}
-
-void Server::handle_accept(Session* new_session, const boost::system::error_code& error){
-    if(!error){
-        new_session->Start();
-    }else{
-        delete new_session;
-    }
-    start_accept();
-}   
 
