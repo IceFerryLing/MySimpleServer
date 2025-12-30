8435cc376112742b6225db3b7dca6e14e2b81d11
IceFerryLing <523918392@qq.com>
2025-12-30 14:33:52 +0800

    MsgNode

---
+++ Async/MsgNode.cpp
@@ -0,0 +1,24 @@
+#include "MsgNode.h"
+#include <cstring>
+#include <iostream>
+using namespace std;
+
+// 构造函数：深拷贝数据到内部缓冲区 _msg
+// msg: 待发送的数据
+// total_len: 数据长度
+MsgNode::MsgNode(const char* msg, int total_len):_total_len(total_len), _cur_len(0){
+        _data = new char[total_len];
+        memcpy(_data, msg, total_len);
+    }
+
+// 构造函数：分配指定长度的缓冲区
+// total_len: 缓冲区大小
+MsgNode::MsgNode(int total_len):_total_len(total_len), _cur_len(0){
+        _data = new char[total_len];
+    }
+
+
+// 析构函数：释放内部缓冲区
+MsgNode::~MsgNode(){
+        delete[] _data;
+    }
