60e66d29b9be1c173f83912542ccdc5609acdfc8
IceFerryLing <523918392@qq.com>
2026-01-17 23:54:37 +0800

    add

---
+++ Async/v2_FullDuplex/LogicNode.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "Singleton.h"
+#include <queue>
+#include <thread>
+#include "Session_demo.h"
+#include <map>
+#include <functional>
+#include "const.h"
+#include "json/json.h"
+#include "json/value.h"
+#include "json/reader.h"
+
+class LogicNode{
+    friend class LogicSystem;
+
+public:
+    LogicNode(shared_ptr<Session>, shared_ptr<RecvNode>);
+
+private:
+    shared_ptr<Session> _session;
+    shared_ptr<RecvNode> _recvNode;
+};
